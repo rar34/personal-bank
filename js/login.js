3f506dae4d4c8d3280eb17ae8502587425a4d72e
rar34 <ra.cse34@gmail.com>
2023-12-06 00:01:20 +0600

    updated email and password

--- js/login.js
+++ js/login.js
@@ -6,7 +6,7 @@ document.getElementById('btn-login').addEventListener('click', function(){
     const passField = document.getElementById('user-password');
     const password = passField.value;
     
-    if(email === "sontan@baap.com" && password === "secret"){
+    if(email === "abc@email.com" && password === "Pass@123"){
         window.location.href = 'bank.html';
     }
     else{
